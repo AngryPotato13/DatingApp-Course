2ac7238b246d70b8e33ac56b1703f1003920062d
Kieran Franklin-Bailey <kieranfb39@gmail.com>
2024-09-26 20:49:37 +0100

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,9 +2,13 @@ using System;
 
 namespace API.Entities;
 
-public class AppUser
+public class AppUser     //elements in the table
 {
     public int Id { get; set; }
 
     public required string UserName { get; set; }
+
+    public required byte[] PasswordHash { get; set; }
+
+    public required byte[] PasswordSalt { get; set; }
 }
